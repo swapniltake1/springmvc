e5d3e178dd1b478a770361bdb75e22e70505d51c
Swapnil Take <takeswapnil8@gmail.com>
2023-02-01 15:08:21 +0530

    first commit

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,23 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller                   // Declaring this class is controller 
+public class HomeController {
+	
+	@RequestMapping("/home")  // when we want to fire this method
+	public String home()
+	{
+		System.out.println("----------hello this home url -------");
+		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String about()
+	{
+		System.out.println("this is about page ");
+		return "about";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home Page</title>
+</head>
+<body>
+
+<h1>THis is home pageee</h1>
+<h2> Called by controoler </h2>
+<h3> url/home</h3>
+
+</body>
+</html>
