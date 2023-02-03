6523659b77f188b07b0f37044b26694e6eec9dc6
Swapnil Take <takeswapnil8@gmail.com>
2023-02-03 18:23:31 +0530

    03-02

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,15 +1,32 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller                   // Declaring this class is controller 
 public class HomeController {
 	
 	@RequestMapping("/home")  // when we want to fire this method
-	public String home()
+	public String home( Model model)
 	{
 		System.out.println("----------hello this home url -------");
+		
+		model.addAttribute("name","Swapnil Rajendra Take");
+		model.addAttribute("Id","101");
+		
+		List <String> friend = new ArrayList<String>();
+		friend.add("Swapnil");
+		friend.add("pooja ");
+		friend.add("abhijit");
+		friend.add("Onkar");
+		friend.add("shradhha");
+		friend.add("rohit");
+		model.addAttribute("f",friend);
+		
 		return "index";
 	}
 	
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -11,6 +12,21 @@
 <h1>THis is home pageee</h1>
 <h2> Called by controoler </h2>
 <h3> url/home</h3>
+
+
+<% 
+ String name = (String)  request.getAttribute("name");
+ Integer id = (Integer)  request.getAttribute("Id");                        
+ List<String> friend=(List<String>) request.getAttribute("f");
+%>
+ <h1> My Name is <%=name %></h1>
+ <h2>  My ID is <%=id %></h2>
+ 
+ <%
+ for (String s: friend){	 
+ %>
+ <h1> My frnds is <%=s%></h1>
+ <% } %>
 
 </body>
 </html>
